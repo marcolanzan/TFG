e20a338e2364b9183d4062071a1c31724a9f5e22
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2024-04-24 13:18:19 +0200

    menu logico (para cambiarlo, hy que tocar el archivo navigation.js)

--- react native party-wars/react-native-party-wars/src/screens/Favorite.js
+++ react native party-wars/react-native-party-wars/src/screens/Favorite.js
@@ -1,11 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, SafeAreaView, StyleSheet } from "react-native";
 
-export default function Favorite() {
+const EventList = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch(
+          "http://192.168.1.90:3000/eventos" // Cambia localhost por tu dirección IP
+        );
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        console.error("El error es " + error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <Text>Favorite</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <FlatList
+        data={events}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.eventItem}>
+            <Text>ID: {item.id}</Text>
+            <Text>Nombre Sala: {item.nombreSala}</Text>
+            <Text>Edad mínima: {item.edadMinEvento}</Text>
+            <Text>Edad máxima: {item.edadMaxEvento}</Text>
+            <Text>Localización: {item.localizacion}</Text>
+            <Text>Temática: {item.tematicaEvento}</Text>
+            <Text>Descripción: {item.descripcionEnvento}</Text>
+            <Text>Localización Evento: {item.localizacionEvento}</Text>
+            <Text>Cantidad de Asistentes: {item.cantidadAsistentes}</Text>
+            <Text>Fecha del Evento: {item.fechaEvento}</Text>
+            <Text>Nombre de la Empresa: {item.nombreEmpEvento}</Text>
+          </View>
+        )}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  eventItem: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+});
+
+export default EventList;
+
+
+
