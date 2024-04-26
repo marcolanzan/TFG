fe095ccce64098490c7875c4af446199ceef0a41
AlbertoRodriguezPenalva <151755187+AlbertoRodriguezPenalva@users.noreply.github.com>
2024-04-26 10:33:46 +0200

    no se si funciona (faltaria el añadir el juego a la sala creada)

--- react native party-wars/react-native-party-wars/src/navigation/Navigation.js
+++ react native party-wars/react-native-party-wars/src/navigation/Navigation.js
@@ -6,7 +6,9 @@ import Account from '../screens/Account';
 import Main from '../screens/Main';
 import LoginScreen from '../screens/Login'; // Importa el componente LoginScreen
 import RegisterScreen from '../screens/RegisterScreen'; // Importa el componente RegisterScreen
-
+import CrearJuego from '../screens/CrearJuego';
+import CrearSala from '../screens/CrearSala';
+import VerJuegos from '../screens/VerJuegos';
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
@@ -52,6 +54,7 @@ export default function Navigation() {
         component={LoginScreen}
         options={{
           tabBarLabel: 'Iniciar Sesión',
+          toBarDisplay: 'none',
           tabBarIcon: ({ color, size }) => (
             <Icon name="sign-in-alt" color={color} size={size} />
           ),
@@ -70,9 +73,37 @@ export default function Navigation() {
           tabBarAccessibilityLabel: 'label-not-visible',
         }}
       />
+      <Tab.Screen
+        name="CrearSala"
+        component = {CrearSala}
+        options ={{
+          tabBarLabel: 'Crear Sala',
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="plus" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+      name = "CrearJuego"
+      component = {CrearJuego}
+      options = {{
+        tabBarLabel: 'Crear Juego',
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="plus" color={color} size={size} />
+        ),
+      }}
+      />
+      <Tab.Screen
+        name="VerJuegos"
+        component={VerJuegos}
+        options={{
+          tabBarLabel: 'Ver Juegos',
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="gamepad" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
-
-
 
